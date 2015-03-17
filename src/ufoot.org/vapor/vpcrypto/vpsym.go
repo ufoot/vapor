8bfc3fa494ae607a7548382b42eedaab6f749b83
Christian Mauduit <ufoot@ufoot.org>
2015-03-17 23:54:16 +0100

    Added symetric encryption/decryption features to vpcrypto package

---
+++ src/ufoot.org/vapor/vpcrypto/vpsym.go
@@ -0,0 +1,98 @@
+// Vapor is a toolkit designed to support Liquid War 7.
+// Copyright (C)  2015  Christian Mauduit <ufoot@ufoot.org>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Vapor homepage: http://www.ufoot.org/liquidwar/v7/vapor
+// Contact author: ufoot@ufoot.org
+
+package vpcrypto
+
+import (
+	"bytes"
+	"compress/gzip"
+	"errors"
+	"golang.org/x/crypto/openpgp"
+	"io"
+	"io/ioutil"
+	"time"
+	"ufoot.org/vapor/vpsys"
+)
+
+// SymEncrypt encrypts a message using a symmetric password/key.
+func SymEncrypt(content, password []byte) ([]byte, error) {
+	var byteWriter bytes.Buffer
+	var err error
+	var output io.WriteCloser
+	var gzipOutput *gzip.Writer
+	var ret []byte
+	var hints openpgp.FileHints
+
+	if len(content) <= 0 {
+		return nil, errors.New("no data")
+	}
+	if len(password) < 16 {
+		return nil, errors.New("password too weak, need at laast 128 bits (16 bytes)")
+	}
+
+	hints.IsBinary = true
+	hints.ModTime = time.Now()
+
+	output, err = openpgp.SymmetricallyEncrypt(&byteWriter, password, &hints, nil)
+	if err != nil {
+		return nil, vpsys.ErrorChain(err, "unable to encrypt content")
+	}
+	gzipOutput = gzip.NewWriter(output)
+	gzipOutput.Write(content)
+	gzipOutput.Close()
+	output.Close()
+
+	ret = byteWriter.Bytes()
+
+	return ret, nil
+}
+
+// SymDecrypt decrypts a message crypted using a symmetric password/key.
+func SymDecrypt(content, password []byte) ([]byte, error) {
+	var err error
+	var byteReader io.Reader
+	var messageDetails *openpgp.MessageDetails
+	var gzipReader *gzip.Reader
+	var ret []byte
+
+	byteReader = bytes.NewReader(content)
+	messageDetails, err = openpgp.ReadMessage(byteReader, nil, func(keys []openpgp.Key, symmetric bool) ([]byte, error) {
+		return password, nil
+	}, nil)
+	if err != nil {
+		return nil, vpsys.ErrorChain(err, "unable to read PGP content")
+	}
+
+	if !messageDetails.IsEncrypted {
+		return nil, errors.New("PGP content was not encrypted")
+	}
+	gzipReader, err = gzip.NewReader(messageDetails.UnverifiedBody)
+	defer gzipReader.Close()
+	if err != nil {
+		return nil, vpsys.ErrorChain(err, "unable to open GZIP within PGP encrypted content")
+	}
+	ret, err = ioutil.ReadAll(gzipReader)
+	if err != nil {
+		return nil, vpsys.ErrorChain(err, "unable to read GZIP within PGP encrypted content")
+	}
+	if len(ret) <= 0 {
+		return nil, vpsys.ErrorChain(err, "no data in GZIP within PGP encrypted content")
+	}
+	return ret, nil
+}
---
+++ src/ufoot.org/vapor/vpcrypto/vpsym_test.go
@@ -0,0 +1,83 @@
+// Vapor is a toolkit designed to support Liquid War 7.
+// Copyright (C)  2015  Christian Mauduit <ufoot@ufoot.org>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Vapor homepage: http://www.ufoot.org/liquidwar/v7/vapor
+// Contact author: ufoot@ufoot.org
+
+package vpcrypto
+
+import (
+	"encoding/hex"
+	"testing"
+	"ufoot.org/vapor/vpsys"
+)
+
+var benchSymPassword []byte
+var benchSymContent []byte
+var benchSymCrypted []byte
+
+func init() {
+	benchSymPassword = []byte("abcdefghijklmnopqrstuvwxyz")
+	benchSymContent = make([]byte, 1500)
+	benchSymCrypted, _ = SymEncrypt(benchSymContent, benchSymPassword)
+}
+
+func TestSym(t *testing.T) {
+	content := []byte("foo bar")
+	password := []byte("0123456789abcdef")
+	var encrypted []byte
+	var decrypted []byte
+	var err error
+
+	encrypted, err = SymEncrypt(content, password)
+	if err == nil {
+		t.Logf("encrypted content=\"%s\" encrypted=\"%s\"", string(content), hex.EncodeToString(encrypted))
+		decrypted, err = SymDecrypt(encrypted, password)
+		if err == nil {
+			t.Logf("decrypted encrypted=\"%s\" decrypted=\"%s\"", hex.EncodeToString(encrypted), string(decrypted))
+			if string(content) != string(decrypted) {
+				t.Error("Content and decrypted differ")
+			}
+		} else {
+			t.Error(err)
+		}
+	} else {
+		t.Error(err)
+	}
+
+}
+
+func BenchmarkSymEncrypt(b *testing.B) {
+	var err error
+
+	for i := 0; i < b.N; i++ {
+		_, err = SymEncrypt(benchSymContent, benchSymPassword)
+		if err != nil {
+			b.Error(vpsys.ErrorChain(err, "unable to encrypt"))
+		}
+	}
+}
+
+func BenchmarkSymDecrypt(b *testing.B) {
+	var err error
+
+	for i := 0; i < b.N; i++ {
+		_, err = SymDecrypt(benchSymCrypted, benchSymPassword)
+		if err != nil {
+			b.Error(vpsys.ErrorChain(err, "unable to decrypt"))
+		}
+	}
+}
