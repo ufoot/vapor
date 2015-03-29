a23a798ef48d6fbb8b0150ee6f5b5d4d9b837c32
Christian Mauduit <ufoot@ufoot.org>
2015-03-29 15:53:59 +0200

    Added tests for rotation matrix

--- vpmatrix3/vpx32mat3_test.go
+++ vpmatrix3/vpx32mat3_test.go
@@ -20,6 +20,7 @@
 package vpmatrix3
 
 import (
+	"github.com/ufoot/vapor/vpmath"
 	"github.com/ufoot/vapor/vpmatrix2"
 	"github.com/ufoot/vapor/vpnumber"
 	"math/rand"
@@ -152,17 +153,36 @@ func TestX32Mat3Aff(t *testing.T) {
 	t.Logf("mat3 MulVec %s * %s = %s", mt.String(), v1.String(), v2.String())
 	v3 := X32Vec3New(p1+t1, p2+t2, vpnumber.X32Const1)
 	if !v2.IsSimilar(v3) {
-		t.Errorf("mat3 MulVec error v2=%s v3=%s", v2.String(), v3.String())
+		t.Errorf("mat3 translation MulVec error v2=%s v3=%s", v2.String(), v3.String())
 	}
 	v2pos := mt.MulVecPos(v1.ToVec2())
 	v3pos := v1.ToVec2().Add(vt)
 	if !v2pos.IsSimilar(v3pos) {
-		t.Errorf("mat3 MulVecPos error v2pos=%s v3pos=%s", v2pos.String(), v3pos.String())
+		t.Errorf("mat3 translation MulVecPos error v2pos=%s v3pos=%s", v2pos.String(), v3pos.String())
 	}
 	v2dir := mt.MulVecDir(v1.ToVec2())
 	v3dir := v1.ToVec2()
 	if !v2dir.IsSimilar(v3dir) {
-		t.Errorf("mat3 MulVecDir error v2dir=%s v3dir=%s", v2dir.String(), v3dir.String())
+		t.Errorf("mat3 translation MulVecDir error v2dir=%s v3dir=%s", v2dir.String(), v3dir.String())
+	}
+
+	mr := X32Mat3Rot(vpmath.X32ConstPi2)
+	t.Logf("rotation mat3 for PI/2 is %s", mr.String())
+	v2 = mr.MulVec(v1)
+	t.Logf("mat3 MulVec %s * %s = %s", mr.String(), v1.String(), v2.String())
+	v3 = X32Vec3New(-v1[1], v1[0], vpnumber.X32Const1)
+	if !v2.IsSimilar(v3) {
+		t.Errorf("mat3 rotation MulVec error v2=%s v3=%s", v2.String(), v3.String())
+	}
+	v2pos = mr.MulVecPos(v1.ToVec2())
+	v3pos = vpmatrix2.X32Vec2New(-v1[1], v1[0])
+	if !v2pos.IsSimilar(v3pos) {
+		t.Errorf("mat3 rotation MulVecPos error v2pos=%s v3pos=%s", v2pos.String(), v3pos.String())
+	}
+	v2dir = mr.MulVecDir(v1.ToVec2())
+	v3dir = vpmatrix2.X32Vec2New(-v1[1], v1[0])
+	if !v2dir.IsSimilar(v3dir) {
+		t.Errorf("mat3 rotation MulVecDir error v2dir=%s v3dir=%s", v2dir.String(), v3dir.String())
 	}
 }
 
