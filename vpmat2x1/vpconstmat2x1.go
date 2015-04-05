d1587494afdbfe803451ec33881b24d2fc3bebfd
Christian Mauduit <ufoot@ufoot.org>
2015-04-05 15:10:21 +0200

    Added consts with matrix elements positions

---
+++ vpmat2x1/vpconstmat2x1.go
@@ -0,0 +1,29 @@
+// Vapor is a toolkit designed to support Liquid War 7.
+// Copyright (C)  2015  Christian Mauduit <ufoot@ufoot.org>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Vapor homepage: https://github.com/ufoot/vapor
+// Contact author: ufoot@ufoot.org
+
+package vpmat2x1
+
+const (
+	// Index of colum 0 row 0 in a 2x1 column-major matrix.
+	Col0Row0 = iota
+	// Index of colum 1 row 0 in a 2x1 column-major matrix.
+	Col1Row0
+	// Number of elements in a 2x1 matrix.
+	Size
+)
