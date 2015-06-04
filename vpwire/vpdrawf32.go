af99b38026ad995b647acf6f7d668a87d4c14a50
Christian Mauduit <ufoot@ufoot.org>
2015-06-04 21:45:12 +0200

    Implementing vpwire

--- vpwire/vpdrawf32.go
+++ vpwire/vpdrawf32.go
@@ -22,6 +22,7 @@ package vpwire
 import (
 	"github.com/ufoot/vapor/vpline3"
 	"github.com/ufoot/vapor/vpmat4x4"
+	"github.com/ufoot/vapor/vpnumber"
 	"github.com/ufoot/vapor/vpvec3"
 	"image"
 	"image/color"
@@ -30,11 +31,31 @@ import (
 // F32Proj calculates the projection for a line, so that all points in the
 // line are visible, given a direction for the center "ray".
 func F32Proj(img *image.RGBA, line *vpline3.F32, dir *vpvec3.F32) *vpmat4x4.F32 {
-	var ret vpmat4x4.F32
+	min := line.Reduce(vpvec3.F32Min)
+	max := line.Reduce(vpvec3.F32Max)
+	avg := line.Reduce(vpvec3.F32Add).DivScale(float32(len(*line)))
 
-	// todo...
+	cameraDirX := vpvec3.F32Normalize(dir)
+	cameraDirZ := vpvec3.F32Cross(cameraDirX, vpvec3.F32AxisY()).Normalize()
+	cameraDirY := vpvec3.F32Cross(cameraDirZ, cameraDirX).Normalize()
+	ret := vpmat4x4.F32RebaseOXYZ(new(vpvec3.F32), cameraDirX, cameraDirY, cameraDirZ)
+	ret.Inv()
 
-	return &ret
+	var diff float32
+	for i, v := range avg {
+		if v-min[i] > diff {
+			diff = v - min[i]
+		}
+		if max[i]-v > diff {
+			diff = max[i] - v
+		}
+	}
+	scale := vpnumber.F32Div(vpnumber.F32Const1, diff)
+	ret.MulComp(vpmat4x4.F32Scale(vpvec3.F32New(scale, scale, scale)))
+
+	ret.MulComp(vpmat4x4.F32Translation(vpvec3.F32Neg(avg)))
+
+	return ret
 }
 
 // F32Draw draws a line on a image,
