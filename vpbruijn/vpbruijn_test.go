f56c5f54ca252ab6bfe90603e8e01aa4040e8618
Christian Mauduit <ufoot@ufoot.org>
2015-11-29 14:37:27 +0100

    Added test for BruijnWalker interface

---
+++ vpbruijn/vpbruijn_test.go
@@ -0,0 +1,120 @@
+// Vapor is a toolkit designed to support Liquid War 7.
+// Copyright (C)  2015  Christian Mauduit <ufoot@ufoot.org>
+//
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// Vapor homepage: https://github.com/ufoot/vapor
+// Contact author: ufoot@ufoot.org
+
+package vpbruijn
+
+import (
+	"encoding/hex"
+	"testing"
+)
+
+var mList = []int{2, 16, 31}
+var nList = []int{3, 64, 113}
+
+func TestBruijnSize(t *testing.T) {
+	for _, m := range mList {
+		for _, n := range nList {
+			t.Logf("testing Bruijn graph walker for m=%d n=%d", m, n)
+			walker, err := BruijnNew(m, n)
+			if err != nil {
+				t.Error("unable to create walker", err)
+			}
+			if walker.M() != m {
+				t.Errorf("M()!=m M()=%d m=%d", walker.M(), m)
+			}
+			if walker.N() != n {
+				t.Errorf("M()!=m M()=%d m=%d", walker.N(), n)
+			}
+			t.Logf("NbBits() -> %d", walker.NbBits())
+			t.Logf("NbBytes() -> %d", walker.NbBytes())
+		}
+	}
+}
+
+func TestBruijnForward(t *testing.T) {
+	for _, m := range mList {
+		for _, n := range nList {
+			t.Logf("testing Bruijn graph walker for m=%d n=%d", m, n)
+			walker, err := BruijnNew(m, n)
+			if err != nil {
+				t.Error("unable to create walker", err)
+			}
+			zero := make([]byte, walker.NbBytes())
+			max := walker.Decr(zero)
+			forwardPath := walker.ForwardPath(zero, max)
+			var lastV []byte
+			for i, v := range forwardPath {
+				if i > 0 {
+					possibleV := walker.NextList(lastV)
+					found := 0
+					for _, w := range possibleV {
+						if walker.Cmp(v, w) == 0 {
+							t.Logf("found key %s as being next to %s", hex.EncodeToString(w), hex.EncodeToString(lastV))
+							found++
+						}
+					}
+					if found == 0 {
+						t.Errorf("no matches for v=%s", hex.EncodeToString(v))
+					}
+					if found > 1 {
+						t.Errorf("too many (%d) matches for v=%s", found, hex.EncodeToString(v))
+					}
+				}
+				lastV = v
+			}
+		}
+	}
+}
+
+/*
+func TestBruijnBackward(t *testing.T) {
+	for _,m:=range mList {
+		for _,n:=range nList {
+			t.Logf("testing Bruijn graph walker for m=%d n=%d",m,n)
+			walker,err:=BruijnNew(m,n)
+			if err!=nil {
+				t.Error("unable to create walker", err)
+			}
+			zero:=make([]byte,walker.NbBytes())
+			max:=walker.Decr(zero)
+			backwardPath:=walker.BackwardPath(zero,max)
+			var lastV []byte
+			for i,v:=range backwardPath {
+				if i>0 {
+					possibleV:=walker.PrevList(lastV)
+					found:=0
+					for _,w:=range(possibleV) {
+						if walker.Cmp(v,w)==0 {
+							t.Logf("found key %s as being prev to %s",hex.EncodeToString(w),hex.EncodeToString(lastV))
+							found++
+						}
+					}
+					if found==0 {
+						t.Errorf("no matches for v=%s",hex.EncodeToString(v))
+					}
+					if found>1 {
+						t.Errorf("too many (%d) matches for v=%s",found,hex.EncodeToString(v))
+					}
+				}
+				lastV=v
+			}
+		}
+	}
+}
+*/
