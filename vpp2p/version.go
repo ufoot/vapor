b6a28537b22fc4c9e29379c71d905929b7d74b1e
Christian Mauduit <ufoot@ufoot.org>
2016-02-22 19:52:05 +0100

    Version bump

--- vpp2p/version.go
+++ vpp2p/version.go
@@ -35,6 +35,6 @@ const PackageLicense = "https://github.com/ufoot/vapor" // PackageLicense set by
 // VersionMajor is the project major version.
 const VersionMajor = 0 // VersionMajor set by stamp.sh
 // VersionMinor is the project minor version.
-const VersionMinor = 18 // VersionMinor set by stamp.sh
+const VersionMinor = 19 // VersionMinor set by stamp.sh
 // VersionStamp is the project stamp, possibly changes for each build.
-const VersionStamp = "c8fc20a" // VersionStamp set by stamp.sh
+const VersionStamp = "b943ddc" // VersionStamp set by stamp.sh
